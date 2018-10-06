387433c34bea80f80a9e407f10ae813a1074dedb
SHARIFUL ISLAM <sharifulcsecou@gmail.com>
2018-10-07 00:35:18 +0600

    Add files via upload

---
+++ SPOJ/TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    while(true)
+    {
+        cin >> N;
+
+        if(N == 42)
+            break;
+
+        cout << N << endl;
+    }
+
+    return 0;
+}
